5fa1299756b03358869895d048a289f3772f56ba
Aditya Raikar <raikaraditya22@gmail.com>
2024-06-06 22:12:56 +0200

    Completed lab

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
+const newRecipe = {
+  title : "Chicken Tandoori",
+  level : "Amateur Chef",
+  ingredients : ['chicken', 'garam masala'],
+  cuisine : "Indian",
+  dishType : 'snack',
+  image : "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  duration: 30,
+  creator:  "aditya"
+}
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
@@ -17,6 +28,26 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create(newRecipe)
+  })
+  .then (recipe => {
+    console.log(`Recipe created: ${recipe.title}`)
+  })
+  .then (() => {
+    return Recipe.insertMany(data)
+  })
+  .then (() => {
+    return Recipe.findOneAndUpdate(
+      {title : 'Rigatoni alla Genovese'},
+      {duration : 100}
+  )})
+  .then (() => {
+    return Recipe.deleteOne(
+      {title: "Carrot Cake"}
+    )
+  })
+  .then (() => {
+    return mongoose.connection.close()
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
